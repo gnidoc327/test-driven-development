cd1350f1c748eae2864c2cdc8481ef22d8a38e3f
Hansung Kim <gnidoc327@gmail.com>
2018-10-30 17:31:34 +0900

    Second test complete for dollar.

--- src/DollarTest.java
+++ src/DollarTest.java
@@ -6,8 +6,10 @@ class DollarTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 
     class Dollar {
@@ -17,8 +19,8 @@ class DollarTest {
             this.amount = amount;
         }
 
-        void times(int multiplier) {
-            amount *= multiplier;
+        Dollar times(int multiplier) {
+            return new Dollar(amount * multiplier);
         }
     }
 }
