57c27e0a3ebc52df3bf9ba4d9759dd4d98b058ea
Hansung Kim <gnidoc327@gmail.com>
2018-10-30 19:51:28 +0900

    Case 8 test complete for money.

--- src/DollarTest.java
+++ src/DollarTest.java
@@ -7,53 +7,79 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class DollarTest {
     @Test
     void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     void testFrancMultiplication() {
-        Franc five = new Franc(5);
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15), five.times(3));
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
     void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
-        assertTrue(new Franc(5).equals(new Franc(5)));
-        assertFalse(new Franc(5).equals(new Franc(6)));
-        assertFalse(new Franc(5).equals(new Dollar(5)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
-    class Money {
-        protected int amount;
+    @Test
+    void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+}
 
-        public boolean equals(Object object) {
-            Money money = (Money) object;
-            return amount == money.amount && this.getClass().equals(money.getClass());
-        }
+abstract class Money {
+    protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
     }
 
-    class Dollar extends Money {
-        Dollar(int amount) {
-            this.amount = amount;
-        }
-
-        Dollar times(int multiplier) {
-            return new Dollar(amount * multiplier);
-        }
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && this.getClass().equals(money.getClass());
     }
 
-    class Franc extends Money {
-        Franc(int amount) {
-            this.amount = amount;
-        }
+    static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
 
-        Franc times(int multiplier) {
-            return new Franc(amount * multiplier);
-        }
+    static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    abstract Money times(int multiplier);
+
+    String currency() {
+        return currency;
+    }
+}
+
+class Dollar extends Money {
+    Dollar(int amount, String currency) {
+        super(amount, currency);
+    }
+
+    Money times(int multiplier) {
+        return Money.dollar(amount * multiplier);
+    }
+}
+
+class Franc extends Money {
+    Franc(int amount, String currency) {
+        super(amount, currency);
+    }
+
+    Money times(int multiplier) {
+        return Money.franc(amount * multiplier);
     }
 }
