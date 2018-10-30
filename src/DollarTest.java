cd93c8b6d156506ead69e0be6c35470d4af846f8
Hansung Kim <gnidoc327@gmail.com>
2018-10-30 17:54:09 +0900

    Case 5 test complete for dollar&Franc.

--- src/DollarTest.java
+++ src/DollarTest.java
@@ -13,6 +13,13 @@ class DollarTest {
     }
 
     @Test
+    void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
     void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
@@ -32,6 +39,23 @@ class DollarTest {
         public boolean equals(Object object) {
             Dollar dollar = (Dollar) object;
             return amount == dollar.amount;
+        }
+    }
+
+    class Franc {
+        private int amount;
+
+        Franc(int amount) {
+            this.amount = amount;
+        }
+
+        Franc times(int multiplier) {
+            return new Franc(amount * multiplier);
+        }
+
+        public boolean equals(Object object) {
+            Franc franc = (Franc) object;
+            return amount == franc.amount;
         }
     }
 }
